9c6fc0285a78d812935a35347e1f19d136b6427e
Shivam <shivamaher100@gmail.com>
2024-06-30 11:29:51 +0530

    cursor-works

--- gram.c
+++ gram.c
@@ -181,7 +181,7 @@ void editormovecursor(int c)
     switch (c)
     {
     case ARROW_UP:
-        if (E.cy > 0)
+        if (E.cy != 0)
             E.cy--;
         break;
     case ARROW_LEFT:
@@ -189,7 +189,7 @@ void editormovecursor(int c)
             E.cx--;
         break;
     case ARROW_DOWN:
-        if (E.cy - 1 < E.screenrows)
+        if (E.cy  < E.screenrows - 1)
             E.cy++;
         break;
     case ARROW_RIGHT:
