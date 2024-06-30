6e81e9f94fed17da57bcd0892f45fcd985b5b777
Shivam <shivamaher100@gmail.com>
2024-06-30 19:03:29 +0530

    misc

--- gram.c
+++ gram.c
@@ -1,3 +1,348 @@
+// // Include Header Files
+
+// #include <unistd.h>
+// #include <termios.h>
+// #include <stdlib.h>
+// #include <ctype.h>
+// #include <stdio.h>
+// #include <signal.h>
+// #include <string.h>
+// #include <errno.h>
+// #include <sys/ioctl.h>
+// #include <sys/types.h>
+// #include <sys/types.h>
+
+// // Defines
+// #define CTRL_KEY(k) ((k) & (0x1f))
+// #define GRAM_VERSION "1.0.0.0"
+
+// enum editorKey
+// {
+//     ARROW_LEFT = 2000,
+//     ARROW_RIGHT,
+//     ARROW_DOWN,
+//     ARROW_UP,
+//     PAGE_UP,
+//     PAGE_DOWN,
+//     DEL_KEY
+// };
+
+// // struct
+// typedef struct erow
+// {
+//     int size;
+//     char *chars;
+
+// } erow;
+// struct editorConfig
+// {
+//     int cx, cy;
+//     int numrows;
+//     erow *row;
+//     int rowoff;
+//     int coloff;
+
+//     int screenrows;
+//     int screencolumns;
+//     struct termios originalstruct;
+// };
+// struct editorConfig E;
+
+// // buffer struct
+// struct abf
+// {
+//     char *b;
+//     int len;
+// };
+// // append Buffer
+
+// void abappend(char *s, int len, struct abf *ptr)
+// {
+//     char *new = realloc(ptr->b, ptr->len + len);
+//     if (new == NULL)
+//         return;
+//     memcpy(&new[ptr->len], s, len);
+//     ptr->b = new;
+//     ptr->len += len;
+// }
+// void abFree(struct abf *ptr)
+// {
+//     // free(ptr);
+// }
+// //  terminal
+// int getwindowsize(int *row, int *col)
+// {
+//     struct winsize ws;
+//     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+//     {
+//         return -1;
+//     }
+//     else
+//     {
+//         *row = ws.ws_row;
+//         *col = ws.ws_col;
+
+//         return 1;
+//     }
+// }
+// void die(const char *s)
+// {
+//     write(STDOUT_FILENO, "\x1b[2J", 4);
+
+//     write(STDOUT_FILENO, "\x1b[H", 3);
+//     perror(s);
+//     exit(1);
+// }
+// void disablerawMode()
+// {
+//     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.originalstruct) == -1)
+//     {
+//         die("tcsetattr");
+//     }
+// }
+// void enablerawMode()
+// {
+//     struct termios raw = E.originalstruct;
+//     atexit(disablerawMode);
+//     tcgetattr(STDIN_FILENO, &raw);
+//     raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+//     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+//     raw.c_cflag |= ~(CS8);
+//     raw.c_oflag &= ~(OPOST);
+//     raw.c_cc[VMIN] = 0;
+//     raw.c_cc[VTIME] = 1;
+//     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+//     {
+//         die("tcsetattr");
+//     }
+// }
+
+// int readkey()
+// {
+//     int nread;
+//     char c;
+//     while ((nread = (read(STDIN_FILENO, &c, 1))) != 1)
+//     {
+//         if (nread == -1 && errno != EAGAIN)
+//         {
+//             die("read");
+//         }
+//     }
+//     if (c == '\x1b')
+//     {
+//         char seq[3];
+
+//         if (read(STDIN_FILENO, &seq[0], 1) != 1)
+//             return '\x1b';
+//         if (read(STDIN_FILENO, &seq[1], 1) != 1)
+//             return '\x1b';
+
+//         // To complete our low-level terminal code, we need to detect a few more special keypresses that use escape sequences, like the arrow keys did. We’ll start with the Page Up and Page Down keys. Page Up is sent as <esc>[5~ and Page Down is sent as <esc>[6~.
+
+//         if (seq[0] == '[')
+//         {
+//             if (seq[1] >= '0' && seq[1] <= '9')
+//             {
+//                 if (read(STDIN_FILENO, &seq[2], 1) != 1)
+//                     return '\x1b';
+//                 if (seq[2] == '~')
+//                 {
+//                     switch (seq[1])
+//                     {
+//                     case '5':
+//                         return PAGE_UP;
+//                         break;
+
+//                     case '6':
+//                         return PAGE_DOWN;
+//                     }
+//                 }
+//             }
+//             else
+//             {
+//                 switch (seq[1])
+//                 {
+//                 case 'A':
+//                     return ARROW_UP;
+//                     break;
+
+//                 case 'B':
+//                     return ARROW_DOWN;
+//                     break;
+
+//                 case 'C':
+//                     return ARROW_RIGHT;
+//                     break;
+
+//                 case 'D':
+//                     return ARROW_LEFT;
+//                     break;
+
+//                 default:
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+//     return c;
+// }
+
+// // Input
+// void editormovecursor(int c)
+// {
+//     erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy + 1];
+//     switch (c)
+//     {
+//     case ARROW_UP:
+//         if (E.cy != 0)
+//         {
+//             E.cy--;
+//         }
+//         break;
+//     case ARROW_LEFT:
+//         if (E.cx != 0)
+//             E.cx--;
+//         break;
+//     case ARROW_DOWN:
+//         if (E.cy <= E.numrows - 1)
+//             E.cy++;
+//         }
+//         break;
+//     case ARROW_RIGHT:
+//         if (row && E.cx < row->size)
+//         {
+//             E.cx++;
+//         }
+//         break;
+//     }
+//     row = (E.cy >= E.numrows)?NULL : &E.row[E.cy+1];
+//     int rowlen = row?row->size:0;
+//     if(E.cx > rowlen)
+//     {
+//         E.cx = rowlen;
+//     }
+// }
+// void editorKeyProcess()
+// {
+//     int c = readkey();
+//     switch (c)
+//     {
+//     case 'q':
+//         exit(0);
+
+//         break;
+//     case PAGE_DOWN:
+//     case PAGE_UP:
+//         // We create a code block with that pair of braces so that we’re allowed to declare the times variable. (You can’t declare variables directly inside a switch statement.)
+//         {
+//             int time = E.screenrows;
+//             while (time--)
+//             {
+//                 editormovecursor(c == PAGE_DOWN ? ARROW_DOWN : ARROW_UP);
+//             }
+//         }
+//         break;
+//     case ARROW_UP:
+//     case ARROW_LEFT:
+//     case ARROW_DOWN:
+//     case ARROW_RIGHT:
+//         editormovecursor(c);
+//         break;
+//     }
+// }
+
+// // Output
+// void editordrawtilde(struct abf *ab)
+// {
+
+//     for (int i = 0; i < E.screenrows; i++)
+//     {
+//         if (i == E.screenrows / 3)
+//         {
+//             char welcome[80];
+//             int welcomelen = snprintf(welcome, sizeof(welcome), "Gram Text Editor %s ", GRAM_VERSION);
+//             if (welcomelen > E.screencolumns)
+//             {
+//                 welcomelen = E.screencolumns;
+//             }
+//             int padding = (E.screencolumns - welcomelen) / 2;
+//             if (padding)
+//             {
+//                 abappend("~", 1, ab);
+//                 padding--;
+//             }
+//             while (padding--)
+//             {
+
+//                     abappend(" ", 1, ab);
+//                 }
+//                 abappend(welcome, welcomelen, ab);
+//                 abappend("\x1b[K", 3, ab);
+//                 abappend("\r\n", 2, ab);
+//             }
+
+//         else if (i < E.screenrows - 1)
+//         {
+//             abappend("~", 1, ab);
+//             abappend("\x1b[K", 3, ab);
+//             abappend("\r\n", 2, ab);
+//         }
+//         else
+//         {
+//             abappend("~", 1, ab);
+//         }
+//     }
+// }
+// #define ABUF_INIT {NULL, 0}
+
+// void editorrefreshscreen()
+// {
+//     struct abf ab = ABUF_INIT;
+//     abappend("\x1b[?25l", 6, &ab);
+//     // The following commmand is used to reposition the cursor at top of the terminal
+//     abappend("\x1b[H", 3, &ab);
+//     editordrawtilde(&ab);
+
+//     char buf[32];
+//     snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
+//     abappend(buf, strlen(buf), &ab);
+
+//     abappend("\x1b[?25h", 6, &ab);
+//     write(STDOUT_FILENO, ab.b, ab.len);
+//     abFree(&ab);
+// }
+
+// // init
+
+// void initeditor()
+// {
+//     E.cx = 0;
+//     E.cy = 0;
+
+//     if (getwindowsize(&E.screenrows, &E.screencolumns) == -1)
+//     {
+//         die("Getwindowsize");
+//     }
+// }
+
+// int main()
+// {
+//     enablerawMode();
+//     initeditor();
+//     if (argc > 1)
+//     {
+//         editoropen(argv[1]);
+//     }
+
+//     char c;
+//     while (1)
+//     {
+//         editorrefreshscreen();
+//         editorKeyProcess();
+//     }
+// }
+
+
 // Include Header Files
 
 #include <unistd.h>
@@ -9,6 +354,7 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <sys/types.h>
 
 // Defines
@@ -36,10 +382,11 @@ typedef struct erow
 struct editorConfig
 {
     int cx, cy;
-    int numrow;
+    int numrows;
+    erow *row;
     int rowoff;
     int coloff;
-    erow *row;
+
     int screenrows;
     int screencolumns;
     struct termios originalstruct;
@@ -188,26 +535,36 @@ int readkey()
 // Input
 void editormovecursor(int c)
 {
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy + 1];
     switch (c)
     {
     case ARROW_UP:
-        if (E.cy > 0)
+        if (E.cy != 0)
+        {
             E.cy--;
+        }
         break;
     case ARROW_LEFT:
-        if (E.cx > 1)
+        if (E.cx != 0)
             E.cx--;
         break;
     case ARROW_DOWN:
-        if (E.cy < E.numrow)
-        {
+        if (E.cy <= E.numrows - 1)
             E.cy++;
         }
         break;
     case ARROW_RIGHT:
-        if (E.cx - 1 < E.screencolumns)
+        if (row && E.cx < row->size)
+        {
             E.cx++;
+        }
         break;
+    }
+    row = (E.cy >= E.numrows)?NULL : &E.row[E.cy+1];
+    int rowlen = row?row->size:0;
+    if(E.cx > rowlen)
+    {
+        E.cx = rowlen;
     }
 }
 void editorKeyProcess()
@@ -240,36 +597,12 @@ void editorKeyProcess()
 }
 
 // Output
-void editorScroll ( )
-{
-    if(E.cy < E.rowoff)
-    {
-        E.rowoff = E.cy;
-
-    }
-    if(E.cy>=E.rowoff+ E.screenrows )
-    {
-        E.rowoff= E.cy - E.screenrows +1;
-    }
-    
-}
 void editordrawtilde(struct abf *ab)
 {
 
     for (int i = 0; i < E.screenrows; i++)
     {
-        int filerow = i + E.rowoff ;
-
-        if (filerow < E.numrow)
-        {
-            int len = E.row[filerow].size;
-            if (len > E.screencolumns)
-                len = E.screencolumns;
-            abappend(E.row[filerow].chars, len, ab);
-            abappend("\x1b[K", 3, ab);
-            abappend("\r\n", 2, ab);
-        }
-        else if (i == E.screenrows / 3 && E.numrow == 0)
+        if (i == E.screenrows / 3)
         {
             char welcome[80];
             int welcomelen = snprintf(welcome, sizeof(welcome), "Gram Text Editor %s ", GRAM_VERSION);
@@ -286,14 +619,14 @@ void editordrawtilde(struct abf *ab)
             while (padding--)
             {
 
-                abappend(" ", 1, ab);
+                    abappend(" ", 1, ab);
+                }
+                abappend(welcome, welcomelen, ab);
+                abappend("\x1b[K", 3, ab);
+                abappend("\r\n", 2, ab);
             }
-            abappend(welcome, welcomelen, ab);
-            abappend("\x1b[K", 3, ab);
-            abappend("\r\n", 2, ab);
-        }
 
-        else if (filerow >= E.numrow)
+        else if (i < E.screenrows - 1)
         {
             abappend("~", 1, ab);
             abappend("\x1b[K", 3, ab);
@@ -309,8 +642,6 @@ void editordrawtilde(struct abf *ab)
 
 void editorrefreshscreen()
 {
-    editorScroll();
-
     struct abf ab = ABUF_INIT;
     abappend("\x1b[?25l", 6, &ab);
     // The following commmand is used to reposition the cursor at top of the terminal
@@ -318,49 +649,12 @@ void editorrefreshscreen()
     editordrawtilde(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
     abappend(buf, strlen(buf), &ab);
 
     abappend("\x1b[?25h", 6, &ab);
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
-}
-// Row Operations
-
-
-void editorappendrow(char *s, size_t len)
-{
-    E.row = realloc(E.row, sizeof(erow) * (E.numrow + 1));
-    int at = E.numrow;
-    E.row[at].size = len;
-    E.row[at].chars = malloc(len + 1);
-    memcpy(E.row[at].chars, s, len);
-    E.row[at].chars[len] = '\0';
-    E.numrow++;
-}
-// file i/o
-
-void editoropen(char *filename)
-{
-    FILE *fl = fopen(filename, "r");
-    if (!fl)
-        die("fopen");
-    char *line = NULL;
-    ssize_t linecap = 0;
-    ssize_t linelen;
-
-    while ((linelen = getline(&line, &linecap, fl)) != -1)
-    {
-        while (linelen > 0 && line[linelen - 1] == '\n' || line[linelen - 1] == '\r')
-        {
-            linelen--;
-        }
-
-        editorappendrow(line, linelen);
-    }
-
-    free(line);
-    fclose(fl);
 }
 
 // init
@@ -369,9 +663,6 @@ void initeditor()
 {
     E.cx = 0;
     E.cy = 0;
-    E.numrow = 0;
-    E.row = NULL;
-    E.rowoff = 0;
 
     if (getwindowsize(&E.screenrows, &E.screencolumns) == -1)
     {
@@ -379,22 +670,7 @@ void initeditor()
     }
 }
 
-// The parameters in the main function of a C program are used to handle command-line arguments passed to the program when it is executed. Here's a breakdown of the parameters:
-
-// int argc:
-
-// argc stands for "argument count".
-// It represents the number of command-line arguments passed to the program, including the program's name itself.
-// For example, if the program is run with ./program file.txt, argc will be 2 (one for ./program and one for file.txt).
-// char *argv[]:
-
-// argv stands for "argument vector".
-// It is an array of character pointers (strings) that point to each of the command-line arguments.
-// argv[0] is the name of the program as it was invoked (e.g., ./program).
-// argv[1] is the first command-line argument passed to the program (e.g., file.txt in the example above).
-// If there are more arguments, they will be stored in argv[2], argv[3], and so on.
-
-int main(int argc, char *argv[])
+int main()
 {
     enablerawMode();
     initeditor();
